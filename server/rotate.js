22e20c68d6e460fcdcab28020f5b89ff6ba2e2a2
Bilal Akhtar <me@itsbilal.com>
2016-09-17 15:10:09 -0400

    Add a simple rotation script that adds income

---
+++ server/rotate.js
@@ -0,0 +1,46 @@
+
+var firebase = require("firebase");
+
+firebase.initializeApp({
+  serviceAccount: "../service_account_creds.json",
+  databaseURL: "https://innovest-393d6.firebaseio.com"
+});
+
+var db = firebase.database();
+
+// Rotator 1: Adds income
+
+var incomeRotator = function(user) {
+  var income = parseFloat(user.child("income").val());
+  var balance = parseFloat(user.child("balance").val()) + income;
+
+  return user.ref.child("balance").set(balance)
+    .then((result) => {
+      return user.ref.once("value");
+    });
+};
+
+var rotators = [incomeRotator];
+
+db.ref("/users/").once("value")
+  .then((users) => {
+    var userPromises = [];
+    users.forEach((user) => {
+      // Send the user through a chain of rotators
+      var promise = Promise.resolve(user);
+
+      for (let rotator of rotators) {
+        promise = promise.then(rotator);
+      }
+
+      userPromises.push(promise);
+    });
+
+    return Promise.all(userPromises);
+  })
+  .then((users) => {
+    console.log("Rotated successfully");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
